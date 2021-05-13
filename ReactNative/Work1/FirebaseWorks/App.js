a40ae9137d5355db643a785e0588e1dd57d889ea
muhosman <78942267+muhosman@users.noreply.github.com>
2021-05-13 21:58:09 +0300

    Osman

---
+++ ReactNative/Work1/FirebaseWorks/App.js
@@ -0,0 +1,11 @@
+ import React, {useState,useEffect} from 'react';
+ import Routes from './Navigation/Routes'
+
+ const App = () => {
+  return(
+    <Routes/>
+  );
+ };
+
+
+ export default App;
---
+++ ReactNative/Work1/FirebaseWorks/Navigation/Routes.js
@@ -0,0 +1,14 @@
+import { NavigationContainer } from '@react-navigation/native';
+import AuthStack from './AuthStack'
+
+
+const Routes = ()=>{
+
+    return(
+        <NavigationContainer>
+            <AuthStack/>
+        </NavigationContainer>
+    );
+}
+
+export default Routes;
